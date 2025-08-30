7ca462e9d5b94de69c90830c7bd30cd69d824776
Shubham <shubhamthakurgta@gmail.com>
2025-08-30 14:17:29 +0530

    Project has started

---
+++ src/main/java/com/shubham/Order_management_saga_project/OrderManagementSagaProjectApplication.java
@@ -0,0 +1,13 @@
+package com.shubham.Order_management_saga_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderManagementSagaProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderManagementSagaProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shubham/Order_management_saga_project/OrderManagementSagaProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shubham.Order_management_saga_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderManagementSagaProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
